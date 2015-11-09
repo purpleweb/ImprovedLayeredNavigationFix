2e6045faac8c364328ded6a4d68adda54dadd2f9
tonioMtl <tonio.cz@gmail.com>
2015-11-09 11:40:28 +0100

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+README
+======
+
+
+Install via Modman
+----------------
+
+You can install this module using [Colin Mollenhour's](https://github.com/colinmollenhour) [Modman tool](https://github.com/colinmollenhour/modman)
+
+```bash
+$ modman init
+$ modman clone https://github.com/tonioMtl/ImprovedLayeredNavigationFix.git
+```
+You can update the module with this command
+
+```bash
+$ modman update ImprovedLayeredNavigationFix
+```
---
+++ app/code/local/MOC/ShopbyFix/Model/Url/Builder.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class MOC_ShopbyFix_Model_Url_Builder extends Amasty_Shopby_Model_Url_Builder
+{
+
+    public function getUrl()
+    {
+        $this->updateEffectiveQuery();
+
+        $paramPart = $this->getParamPart();
+        $basePart = $this->getBasePart($paramPart);
+
+        $url = $basePart . $paramPart;
+
+        if( !empty($paramPart) ){
+            $url .= '/';
+        }
+
+        $url = preg_replace('|(^:)/{2,}|', '$1/', $url);
+        //print("#".$paramPart .' @ '.$url."   <br/>\n");
+
+        return $url;
+    }
+
+}
+
