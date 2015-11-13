f6a2c00f92309b0a0274108b536caab6297bfbd1
Tonio <tonio.cz@gmail.com>
2015-11-13 17:30:34 +0100

    remove slash in url like ?order=24/

--- app/code/local/MOC/ShopbyFix/Model/Url/Builder.php
+++ app/code/local/MOC/ShopbyFix/Model/Url/Builder.php
@@ -13,8 +13,18 @@ class MOC_ShopbyFix_Model_Url_Builder extends Amasty_Shopby_Model_Url_Builder
 
         $url = $basePart . $paramPart;
 
+        //Mage::log('url   : '.$url, null, 'url-builder.log', true);
+        //Mage::log('base  : '.$basePart, null, 'url-builder.log', true);
+        Mage::log('param : '.$paramPart, null, 'url-builder.log', true);
+
         if( !empty($paramPart) ){
-            $url .= '/';
+            // if ? then no trailling slash
+            if (strpos($paramPart,'?') !== false) {
+                // nothing
+            } else {
+                // else add a slash
+                $url .= '/';
+            }
         }
 
         $url = preg_replace('|(^:)/{2,}|', '$1/', $url);
